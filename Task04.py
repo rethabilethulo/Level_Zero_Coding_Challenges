0c27af39a97e177570496b8e7742973aa3d7a43d
rethabilethulo <thulorethabile@gmail.com>
2021-09-14 12:46:36 +0200

    Added Task 0.4

---
+++ Task04.py
@@ -0,0 +1,9 @@
+def even_or_odd(number):
+    if number % 2 == 0:
+        print(number, "is a even number")
+    else:
+        print(number, "is a odd number")
+
+""" main code """
+even_or_odd(3)
+even_or_odd(8)
